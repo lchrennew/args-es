e0343e61f53cafde75e908cc77c5a9e1633fe09a
lichun <lchrennew@126.com>
2021-12-10 22:19:53 +0800

    opt: 增加私有访问限制

--- src/args-parser.js
+++ src/args-parser.js
@@ -1,38 +1,38 @@
 export default class ArgsParser {
-    schema
+    #schema
 
     constructor(schema) {
-        this.schema = schema
+        this.#schema = schema
     }
 
     parse(argsString) {
         const args = {}
-        const append = arg => this.schema.typeOf(arg.flag).appendTo(args, arg)
+        const append = arg => this.#schema.typeOf(arg.flag).appendTo(args, arg)
         const parts = argsString.split(' ').filter(part => part)
 
         for (let i = 0; i < parts.length; i++) {
-            const flag = this.parseFlag(parts[i])
-            const value = this.parseValue(flag, parts[i + 1])
+            const flag = this.#parseFlag(parts[i])
+            const value = this.#parseValue(flag, parts[i + 1])
             const isValueFlag = value === undefined
             if (!isValueFlag) i++
             append({ flag, value })
         }
 
-        return { ...this.schema.defaults(), ...args }
+        return { ...this.#schema.defaults(), ...args }
     }
 
-    validateFlag(part) {
-        return part?.match(/^-.$/) && this.schema.defined(part[1])
+    #validateFlag(part) {
+        return part?.match(/^-.$/) && this.#schema.defined(part[1])
     }
 
-    parseFlag(part) {
-        if (!this.validateFlag(part)) throw `Invalid flag: ${part}`
+    #parseFlag(part) {
+        if (!this.#validateFlag(part)) throw `Invalid flag: ${part}`
         return part[1]
     }
 
-    parseValue(flag, part) {
-        if (this.validateFlag(part)) return
-        const type = this.schema.typeOf(flag)
-        return type.parse(part, this.schema.defaultOf(flag))
+    #parseValue(flag, part) {
+        if (this.#validateFlag(part)) return
+        const type = this.#schema.typeOf(flag)
+        return type.parse(part, this.#schema.defaultOf(flag))
     }
 }
--- src/args-schema.js
+++ src/args-schema.js
@@ -1,34 +1,34 @@
 export default class ArgsSchema {
-    definition
+    #definition
 
     constructor(definition) {
-        this.definition = definition
+        this.#definition = definition
     }
 
     defined(flag) {
-        return !!this.definition[flag]
+        return !!this.#definition[flag]
     }
 
     typeOf(flag) {
-        return this.definition[flag].type
+        return this.#definition[flag].type
     }
 
     defaultOf(flag) {
-        return this.definition[flag].default ?? this.definition[flag].type.globalDefault
+        return this.#definition[flag].default ?? this.#definition[flag].type.globalDefault
     }
 
     defaults() {
         return Object.fromEntries(
             Object
-                .entries(this.definition)
+                .entries(this.#definition)
                 .map(entry => [ entry[0], this.defaultOf(entry[0]) ]))
     }
 
     manual() {
         console.log(``)
         console.log(`Flags:`)
-        for (const flag in this.definition) {
-            const def = this.definition[flag]
+        for (const flag in this.#definition) {
+            const def = this.#definition[flag]
             console.log(`  -${flag}  <${this.typeOf(flag).name}>\t  ${def.description} (default = ${JSON.stringify(this.defaultOf(flag))})`)
         }
     }
